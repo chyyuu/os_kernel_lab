2f904a130697a3bb27360214e89b98689a6aae1d
yuchen <yuchen@tsinghua.edu.cn>
2015-03-23 09:11:45 +0800

    add linux little progs for locality

---
+++ related_info/lab3/locality/goodlocality.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define NUM 1024
+#define COUNT 10
+int A[NUM][NUM];
+void main (void) {
+  int i,j,k;
+  for (k = 0; k<COUNT; k++)
+  for (i = 0; i < NUM; i++)
+  for (j = 0; j	 < NUM; j++)
+      A[i][j] = i+j;
+  printf("%d count computing over!\n",i*j*k);
+}
