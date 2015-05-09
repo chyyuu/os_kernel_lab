1e3bab2bab0f515bb6b72978a5a7b35876b08374
ArchStacker <ArchStacker@gmail.com>
2015-05-10 00:37:10 +0800

    Fix the bug in lab4_result:vmm.c

--- labcodes_answer/lab4_result/kern/mm/vmm.c
+++ labcodes_answer/lab4_result/kern/mm/vmm.c
@@ -420,6 +420,7 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
             }    
             page_insert(mm->pgdir, page, addr, perm);
             swap_map_swappable(mm, addr, page, 1);
+            page->pra_vaddr = addr;
         }
         else {
             cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
