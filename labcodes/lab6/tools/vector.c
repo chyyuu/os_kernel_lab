ae8709b25c590ce3b11d98f6b7251987c2e2793f
xu0o0 <hqcat6@gmail.com>
2017-07-17 04:16:25 +0800

    fix bug in tools/vector.c and kern/trap/vector.S
    Interrupt 9 has no exception error code.
    Reference: 6.15 of Intel® 64 and IA-32 Architectures Software Developer’s Manual

--- labcodes/lab6/tools/vector.c
+++ labcodes/lab6/tools/vector.c
@@ -10,7 +10,7 @@ main(void) {
     for (i = 0; i < 256; i ++) {
         printf(".globl vector%d\n", i);
         printf("vector%d:\n", i);
-        if ((i < 8 || i > 14) && i != 17) {
+        if (i != 8 && (i < 10 || i > 14) && i != 17) {
             printf("  pushl $0\n");
         }
         printf("  pushl $%d\n", i);
