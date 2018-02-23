08c9fc1bc043ed1f733178580998c5ddaa71653c
Wende Tan <twd2@163.com>
2018-02-23 16:53:48 +0800

    lab3: ex2

--- labcodes/lab3/kern/init/init.c
+++ labcodes/lab3/kern/init/init.c
@@ -46,7 +46,7 @@ kern_init(void) {
 
     //LAB1: CAHLLENGE 1 If you try to do it, uncomment lab1_switch_test()
     // user/kernel mode switch test
-    lab1_switch_test();
+    //lab1_switch_test();
 
     /* do nothing */
     while (1);
