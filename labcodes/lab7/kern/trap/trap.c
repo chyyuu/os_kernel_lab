107dfc605b4cc55482fc2f12cb398a910dafd20a
Wende Tan <twd2@163.com>
2018-02-24 14:35:38 +0800

    lab7: ex1

--- labcodes/lab7/kern/trap/trap.c
+++ labcodes/lab7/kern/trap/trap.c
@@ -241,12 +241,12 @@ trap_dispatch(struct trapframe *tf) {
          * IMPORTANT FUNCTIONS:
 	     * sched_class_proc_tick
          */         
-        sched_class_proc_tick(current);
         /* LAB7 YOUR CODE */
         /* you should upate you lab6 code
          * IMPORTANT FUNCTIONS:
 	     * run_timer_list
          */
+        run_timer_list();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
