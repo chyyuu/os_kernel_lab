10530322431fb0a7a644987af8bf9f4f06bf8c04
Wende Tan <twd2@163.com>
2018-02-24 13:39:08 +0800

    lab6: update lab5

--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -232,18 +232,16 @@ trap_dispatch(struct trapframe *tf) {
          * (3) Too Simple? Yes, I think so!
          */
         ++ticks;
-        if (ticks % TICK_NUM == 0) {
-            current->need_resched = 1;
-        }
         /* LAB5 2015011278 */
         /* you should upate you lab1 code (just add ONE or TWO lines of code):
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
          */
-        /* LAB6 YOUR CODE */
+        /* LAB6 2015011278 */
         /* you should upate you lab5 code
          * IMPORTANT FUNCTIONS:
 	     * sched_class_proc_tick
          */
+        sched_class_proc_tick(current);
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
