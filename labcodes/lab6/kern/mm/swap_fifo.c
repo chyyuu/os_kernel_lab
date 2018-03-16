d941f65e5d14eb58cb1157b00974118dd1f178b3
chyyuu <yuchen@tsinghua.edu.cn>
2018-03-16 09:04:26 +0800

    update comments in swap_fifo.c

--- labcodes/lab6/kern/mm/swap_fifo.c
+++ labcodes/lab6/kern/mm/swap_fifo.c
@@ -55,7 +55,7 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
 }
 /*
  *  (4)_fifo_swap_out_victim: According FIFO PRA, we should unlink the  earliest arrival page in front of pra_list_head qeueue,
- *                            then set the addr of addr of this page to ptr_page.
+ *                            then assign the value of *ptr_page to the addr of this page.
  */
 static int
 _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
@@ -66,7 +66,7 @@ _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick
      /* Select the victim */
      /*LAB3 EXERCISE 2: YOUR CODE*/ 
      //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
-     //(2)  set the addr of addr of this page to ptr_page
+     //(2)  assign the value of *ptr_page to the addr of this page
      return 0;
 }
 
--- labcodes_answer/lab5_result/kern/mm/swap_fifo.c
+++ labcodes_answer/lab5_result/kern/mm/swap_fifo.c
@@ -56,7 +56,7 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
 }
 /*
  *  (4)_fifo_swap_out_victim: According FIFO PRA, we should unlink the  earliest arrival page in front of pra_list_head qeueue,
- *                            then set the addr of addr of this page to ptr_page.
+ *                            then assign the value of *ptr_page to the addr of this page.
  */
 static int
 _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
@@ -67,7 +67,7 @@ _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick
      /* Select the victim */
      /*LAB3 EXERCISE 2: YOUR CODE*/ 
      //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
-     //(2)  set the addr of addr of this page to ptr_page
+     //(2)  assign the value of *ptr_page to the addr of this page
      /* Select the tail */
      list_entry_t *le = head->prev;
      assert(head!=le);
