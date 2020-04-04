d4a043dddd5a552ede5debdf3784608f9c0496f8
涂轶翔 <tyx17@mails.tsinghua.edu.cn>
2020-04-05 02:47:21 +0800

    添加最基础部分（运行操作系统）的代码和注释 #2

---
+++ os/src/panic.rs
@@ -0,0 +1,25 @@
+//! 代替 std 库，实现 panic 和 abort 的功能
+
+use core::panic::PanicInfo;
+use crate::sbi::shutdown;
+
+/// 打印 panic 的信息并 [`shutdown`]
+///
+/// ### `#[panic_handler]` 属性
+/// 声明此函数是 panic 的回调
+#[panic_handler]
+fn panic_handler(info: &PanicInfo) -> ! {
+    // `\x1b[??m` 是控制终端字符输出格式的指令，在支持的平台上可以改变文字颜色等等
+    // 这里使用错误红
+    // 参考：https://misc.flogisoft.com/bash/tip_colors_and_formatting
+    println!("\x1b[1;31mpanic: '{}'\x1b[0m", info.message().unwrap());
+    shutdown()
+}
+
+/// 终止程序
+/// 
+/// 调用 [`panic_handler`]
+#[no_mangle]
+extern "C" fn abort() -> ! {
+    panic!("abort()")
+}
---
+++ os/src/sbi.rs
@@ -0,0 +1,47 @@
+//! 调用 Machine 层的操作
+// 目前还不会用到全部的 SBI 调用，暂时允许未使用的变量或函数
+#![allow(unused)]
+
+/// SBI 调用
+#[inline(always)]
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let ret;
+    unsafe {
+        asm!("ecall"
+            : "={x10}" (ret)
+            : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+            : "memory"
+            : "volatile");
+    }
+    ret
+}
+
+const SBI_SET_TIMER: usize = 0;
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+const SBI_CONSOLE_GETCHAR: usize = 2;
+const SBI_CLEAR_IPI: usize = 3;
+const SBI_SEND_IPI: usize = 4;
+const SBI_REMOTE_FENCE_I: usize = 5;
+const SBI_REMOTE_SFENCE_VMA: usize = 6;
+const SBI_REMOTE_SFENCE_VMA_ASID: usize = 7;
+const SBI_SHUTDOWN: usize = 8;
+
+/// 向控制台输出一个字符
+///
+/// 需要注意我们不能直接使用 Rust 中的 char 类型
+pub fn console_putchar(c: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
+}
+
+/// 从控制台中读取一个字符
+///
+/// 没有读取到字符则返回 -1
+pub fn console_getchar() -> usize {
+    sbi_call(SBI_CONSOLE_GETCHAR, 0, 0, 0)
+}
+
+/// 调用 SBI_SHUTDOWN 来关闭操作系统（直接退出 QEMU）
+pub fn shutdown() -> ! {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    unreachable!()
+}
