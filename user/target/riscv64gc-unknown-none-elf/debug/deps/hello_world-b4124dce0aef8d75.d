d1fcf2fc9fe3cc594885f39c21b543a753006c8a
Yifan Wu <shinbokuow@163.com>
2020-11-18 14:53:47 +0800

    Removed unused cache.

--- user/target/riscv64gc-unknown-none-elf/debug/deps/hello_world-b4124dce0aef8d75.d
+++
@@ -1,5 +0,0 @@
-/home/oslab/workspace/v3/rCore-Tutorial-v3/user/target/riscv64gc-unknown-none-elf/debug/deps/hello_world-b4124dce0aef8d75: src/bin/hello_world.rs
-
-/home/oslab/workspace/v3/rCore-Tutorial-v3/user/target/riscv64gc-unknown-none-elf/debug/deps/hello_world-b4124dce0aef8d75.d: src/bin/hello_world.rs
-
-src/bin/hello_world.rs:
