8cc6b584ad540795eb40dbe1a154ff540ac1b661
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2020-12-29 22:35:12 +0800

    add smallest runtime env, and cargo build successfully

---
+++ os/src/entry.asm
@@ -0,0 +1,12 @@
+    .section .text.entry
+    .globl _start
+_start:
+    la sp, boot_stack_top
+    call rust_main
+
+    .section .bss.stack
+    .globl boot_stack
+boot_stack:
+    .space 4096 * 16
+    .globl boot_stack_top
+boot_stack_top:
