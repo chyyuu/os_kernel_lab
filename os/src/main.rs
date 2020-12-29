766fae0b68236ee34403fa81c69697766d9207af
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2020-12-29 22:22:33 +0800

    comment println!, and add panic_handler to bypass two errs, but got a new err "requires `start` lang_item"

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,4 +1,12 @@
 #![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
 }
