79e25198920b5123dcdef4366e489d3a0936f376
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-01-02 22:30:30 +0800

    add sys_yield syscall interface in user lib

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -2,6 +2,7 @@ pub const STDOUT: usize = 1;
 
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
+const SYSCALL_YIELD: usize = 124;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -22,4 +23,8 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
 
 pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+pub fn sys_yield() -> isize {
+    syscall(SYSCALL_YIELD, [0, 0, 0])
 }
