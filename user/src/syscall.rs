a80f71ac4c329b5d0eb0ebbe1c5434422e2e2b9d
Yifan Wu <shinbokuow@163.com>
2021-01-06 00:05:21 +0800

    Remove unused sys_get_time

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -1,7 +1,6 @@
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
-const SYSCALL_GET_TIME: usize = 169;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -26,8 +25,4 @@ pub fn sys_exit(exit_code: i32) -> isize {
 
 pub fn sys_yield() -> isize {
     syscall(SYSCALL_YIELD, [0, 0, 0])
-}
-
-pub fn sys_get_time() -> isize {
-    syscall(SYSCALL_GET_TIME, [0, 0, 0])
 }
