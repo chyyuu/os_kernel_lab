76427ced57df85aadc8a5c47e1ae6133755905ba
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-01-05 17:37:53 +0800

    Update userland app without std, but got compiling err"no panic handler definition"
    chyyuu:os$ cargo build
       Compiling os v0.1.0 (/media/chyyuu/ca8c7ba6-51b7-41fc-8430-e29e31e5328f/thecode/rust/os_kernel_lab/os)
    error: language item required, but not found: `eh_personality`
    
    error: aborting due to previous error
    
    error: could not compile `os`
    
    To learn more, run the command again with --verbose.
    chyyuu:os$ cargo rustc -- -C link-arg=-nostartfiles
       Compiling os v0.1.0 (/media/chyyuu/ca8c7ba6-51b7-41fc-8430-e29e31e5328f/thecode/rust/os_kernel_lab/os)
    error: language item required, but not found: `eh_personality`
    
    error: aborting due to previous error
    
    error: could not compile `os`
    
    To learn more, run the command again with --verbose.

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+extern "C" fn _start() {
+    loop {}
 }
