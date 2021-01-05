3e5f44e2deb0ff3f83734d30dc0c589486c33d11
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-01-05 19:28:24 +0800

    update for mini-env of userland app, but got running err.
    qemu-riscv64 target/riscv64gc-unknown-none-elf/release/os
    段错误 (核心已转储)

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,14 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(global_asm)]
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub extern "C" fn rust_main() -> ! {
+    loop {}
+}
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[no_mangle]
-extern "C" fn _start() {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
