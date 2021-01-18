cfed8cb75cd1676e310b89577e743dfebe14a2e0
Yifan Wu <shinbokuow@163.com>
2021-01-18 21:31:55 +0800

    xstate -> exit_code

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 use crate::batch::run_next_app;
 
 pub fn sys_exit(exit_code: i32) -> ! {
-    println!("[kernel] Application exited with code {}", xstate);
+    println!("[kernel] Application exited with code {}", exit_code);
     run_next_app()
 }
