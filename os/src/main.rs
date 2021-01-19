4961306dc8d098859aee831398ec8043252a09a6
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-01-19 23:20:21 +0800

    add print, but run error

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,15 +2,18 @@
 #![no_main]
 #![feature(llvm_asm)]
 #![feature(global_asm)]
-global_asm!(include_str!("entry.asm"));
+//global_asm!(include_str!("entry.asm"));
 
 use core::panic::PanicInfo;
+use core::fmt::{self, Write};
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
+const STDOUT: usize = 1;
+const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SBI_SHUTDOWN: usize = 8;
 
@@ -36,9 +39,42 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(STDOUT, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
 #[no_mangle]
 #[link_section=".text.entry"]
-extern "C" fn rust_main() {
-    //sys_exit(9);
-    shutdown();
+//extern "C" fn rust_main() {
+extern "C" fn _start() {
+    //println!("Hello, world!");
+    sys_exit(9);
+    //shutdown();
 }
