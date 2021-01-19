cef69cf548170ab1a673e930f3c789cb9b8ff1ee
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-01-19 13:01:39 +0800

    add sys exit, now 'qemu-riscv64 target/riscv64gc-unknown-none-elf/debug/os' can finish normally

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(llvm_asm)]
 
 use core::panic::PanicInfo;
 
@@ -8,7 +9,27 @@ fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
+const SYSCALL_EXIT: usize = 93;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        llvm_asm!("ecall"
+            : "={x10}" (ret)
+            : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
+            : "memory"
+            : "volatile"
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
 #[no_mangle]
 extern "C" fn _start() {
-    loop {}
+    sys_exit(0);
+    //loop{};
 }
