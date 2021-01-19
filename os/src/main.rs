998a6fbc9cade4578476275907874dc1656ad81a
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-01-19 13:06:18 +0800

    add print support in minimal runtime.

--- os/src/main.rs
+++ os/src/main.rs
@@ -3,13 +3,17 @@
 #![feature(llvm_asm)]
 
 use core::panic::PanicInfo;
+use core::fmt::{self, Write};
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
+const STDOUT: usize = 1;
+const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
+
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -28,8 +32,41 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(STDOUT, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+
 #[no_mangle]
 extern "C" fn _start() {
+    println!("Hello, world!");
     sys_exit(0);
     //loop{};
 }
