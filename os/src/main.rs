f6fb60288a6b422090d9e23c641038d34a3efcb7
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-02-03 15:21:21 +0800

    add interrupt support, but got error
    $./run.sh
    
    [rustsbi] Version 0.1.0
    .______       __    __      _______.___________.  _______..______   __
    |   _  \     |  |  |  |    /       |           | /       ||   _  \ |  |
    |  |_)  |    |  |  |  |   |   (----`---|  |----`|   (----`|  |_)  ||  |
    |      /     |  |  |  |    \   \       |  |      \   \    |   _  < |  |
    |  |\  \----.|  `--'  |.----)   |      |  |  .----)   |   |  |_)  ||  |
    | _| `._____| \______/ |_______/       |__|  |_______/    |______/ |__|
    
    [rustsbi] Platform: QEMU
    [rustsbi] misa: RV64ACDFIMSU
    [rustsbi] mideleg: 0x222
    [rustsbi] medeleg: 0xb1ab
    [rustsbi] Kernel entry: 0x80020000
    Kernel: Hello, world!
    Panicked at src/main.rs:166 Unsupported trap Exception(StoreFault), stval = 0xfffffffffffffef8, sepc = 0x800200c2

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,37 +4,52 @@
 #![feature(global_asm)]
 #![feature(panic_info_message)]
 
+//=====================SHARE PARTS============================
+
+static mut CLOCKNUM:u64=0;
+//======================= SUPERVISOR MODE =========================
+
+//================== entry point ====================
 global_asm!(include_str!("entry.asm"));
 
-use core::panic::PanicInfo;
+// ================ panic  handler ==================
 use core::fmt::{self, Write};
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
-        println!("Panicked at {}:{} {}", location.file(), location.line(), info.message().unwrap());
+        kprintln!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
     } else {
-        println!("Panicked: {}", info.message().unwrap());
+        kprintln!("Panicked: {}", info.message().unwrap());
     }
     shutdown()
 }
 
-const STDOUT: usize = 1;
-const SYSCALL_WRITE: usize = 64;
-const SYSCALL_EXIT: usize = 93;
+// ================== SBI call ===============
 const SBI_CONSOLE_PUTCHAR: usize = 1;
 const SBI_SHUTDOWN: usize = 8;
+const SBI_SET_TIMER: usize = 0;
 
 pub fn console_putchar(c: usize) {
-    syscall(SBI_CONSOLE_PUTCHAR, [c, 0, 0]);
+    sbicall(SBI_CONSOLE_PUTCHAR, [c, 0, 0]);
 }
 
 pub fn shutdown() -> ! {
-    syscall(SBI_SHUTDOWN, [0, 0, 0]);
+    sbicall(SBI_SHUTDOWN, [0, 0, 0]);
     panic!("It should shutdown!");
 }
 
-fn syscall(id: usize, args: [usize; 3]) -> isize {
+pub fn set_timer(timer: usize) {
+    sbicall(SBI_SET_TIMER, [timer, 0, 0]);
+}
+
+fn sbicall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
     unsafe {
         llvm_asm!("ecall"
@@ -47,19 +62,11 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
     ret
 }
 
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
+//===============kernel mode console ========================
+struct Kstdout;
 
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-struct Stdout;
-
-impl Write for Stdout {
+impl Write for Kstdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        //sys_write(STDOUT, s.as_bytes());
         for c in s.chars() {
             console_putchar(c as usize);
         }
@@ -67,30 +74,115 @@ impl Write for Stdout {
     }
 }
 
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
+pub fn kconsole_print(args: fmt::Arguments) {
+    Kstdout.write_fmt(args).unwrap();
 }
 
 #[macro_export]
-macro_rules! print {
+macro_rules! kprint {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        kconsole_print(format_args!($fmt $(, $($arg)+)?));
     }
 }
 
 #[macro_export]
-macro_rules! println {
+macro_rules! kprintln {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+        kconsole_print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
 }
+
+//===============rv registers======================
+use riscv::register::{
+    mtvec::TrapMode,
+    scause::{self, Trap, Interrupt},
+    sstatus::{self, Sstatus, SPP},
+    stval, stvec,sie, sepc,
+    time,
+};
+
+//========= timer device =========================
+pub fn enable_timer_interrupt() {
+    unsafe {
+        sie::set_stimer();
+        sstatus::set_sie();
+    }
+}
+
+const TICKS_PER_SEC: usize = 100;
+const MSEC_PER_SEC: usize = 1000;
+const CLOCK_FREQ: usize = 12500000;
+
+pub fn get_time() -> usize {
+    time::read()
+}
+
+pub fn get_time_ms() -> usize {
+    time::read() / (CLOCK_FREQ / MSEC_PER_SEC)
+}
+
+pub fn set_next_trigger() {
+    set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
+}
+
+//================= for trap ===============================
+
+// TrapContext needs 34*8 bytes
+#[repr(C)]
+pub struct TrapContext {
+    pub x: [usize; 32],
+    pub sstatus: Sstatus,
+    pub sepc: usize,
+}
+
+
+// __alltraps & __restore functions
+global_asm!(include_str!("trap.S"));
+
+pub fn trap_init() {
+    extern "C" {
+        fn __alltraps(); //in trap.S
+    }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
+
 
 #[no_mangle]
-#[link_section=".text.entry"]
+pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
+    let scause = scause::read();
+    let stval = stval::read();
+    let sepc = sepc::read();
+
+    match scause.cause() {
+        // timer interrupt
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            kprintln!("clock");
+            set_next_trigger();
+            //unsafe {CLOCKNUM+=1;}
+        }
+        _ => {
+            panic!(
+                "Unsupported trap {:?}, stval = {:#x}, sepc = {:#x}",
+                scause.cause(),
+                stval, sepc
+            );
+        }
+    }
+    cx
+}
+
+
+//================ kernel main =============================
+#[no_mangle]
+#[link_section = ".text.entry"]
 extern "C" fn rust_main() {
-//extern "C" fn _start() {
-    println!("Hello, world!");
-    //sys_exit(9);
-    //shutdown();
-    panic!("It should shutdown!");
+    kprintln!("Kernel: Hello, world!");
+    trap_init();
+    set_next_trigger();
+    enable_timer_interrupt();
+    loop{
+        //unsafe {kprintln!("clock num is {}",CLOCKNUM);}
+    };
 }
