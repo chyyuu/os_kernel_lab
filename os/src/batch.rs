e9717e1052e21a9e9facf5beca40514d9feee070
Yifan Wu <shinbokuow@163.com>
2021-02-07 17:28:26 +0800

    Bump rustsbi to 0.1.1 && make config of qemu/k210 different

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -5,7 +5,7 @@ use crate::trap::TrapContext;
 const USER_STACK_SIZE: usize = 4096 * 2;
 const KERNEL_STACK_SIZE: usize = 4096 * 2;
 const MAX_APP_NUM: usize = 16;
-const APP_BASE_ADDRESS: usize = 0x80040000;
+const APP_BASE_ADDRESS: usize = 0x80400000;
 const APP_SIZE_LIMIT: usize = 0x20000;
 
 #[repr(align(4096))]
