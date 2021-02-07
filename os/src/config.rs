982b369a516f45300b2d254d231f94cce9fcf2a5
Yifan Wu <shinbokuow@163.com>
2021-02-07 18:31:18 +0800

    Bump rustsbi to 0.1.1 && make config of qemu/k210 different

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,7 +1,13 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x20_0000;
+
+#[cfg(feature = "board_k210")]
 pub const MEMORY_END: usize = 0x80600000;
+
+#[cfg(feature = "board_qemu")]
+pub const MEMORY_END: usize = 0x80800000;
+
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
 
