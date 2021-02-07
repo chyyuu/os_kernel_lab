eb1259ecd9e07d3ca8211109df371e6b63f5a6d3
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-02-07 10:34:53 +0800

    add page-grained allocator

--- os/run.sh
+++ os/run.sh
@@ -1,10 +1,6 @@
+cargo clean
 cargo build --release 
-rust-objcopy --binary-architecture=riscv64 target/riscv64gc-unknown-none-elf/release/os --strip-all -O binary target/riscv64gc-unknown-none-elf/release/os.bin
-qemu-system-riscv64 \
-	-machine virt \
-	-nographic \
-	-bios ../bootloader/rustsbi-qemu.bin \
-	-device loader,file=target/riscv64gc-unknown-none-elf/release/os.bin,addr=0x80020000 
+qemu-system-riscv64 -machine virt -m 8 -nographic -bios ../bootloader/rustsbi-qemu.bin  -kernel target/riscv64gc-unknown-none-elf/release/os
  
  
  
--- os/src/main.rs
+++ os/src/main.rs
@@ -63,7 +63,7 @@ pub fn print(args: fmt::Arguments) {
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        print(format_args!($fmt $(, $($arg)+)?));
     }
 }
 
@@ -73,6 +73,309 @@ macro_rules! println {
         print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
 }
+
+//----------------- page-graind allocator -------------------
+use core::{mem::size_of, ptr::null_mut};
+
+static mut HEAP_START: usize = 0;
+static mut HEAP_SIZE: usize =  0;
+
+// We will use ALLOC_START to mark the start of the actual
+// memory we can dish out.
+static mut ALLOC_START: usize = 0;
+const PAGE_ORDER: usize = 12;
+pub const PAGE_SIZE: usize = 1 << 12;
+
+/// Align (set to a multiple of some power of two)
+/// This takes an order which is the exponent to 2^order
+/// Therefore, all alignments must be made as a power of two.
+/// This function always rounds up.
+pub const fn align_val(val: usize, order: usize) -> usize {
+    let o = (1usize << order) - 1;
+    (val + o) & !o
+}
+
+#[repr(u8)]
+pub enum PageBits {
+    Empty = 0,
+    Taken = 1 << 0,
+    Last = 1 << 1,
+}
+
+impl PageBits {
+    // We convert PageBits to a u8 a lot, so this is
+    // for convenience.
+    pub fn val(self) -> u8 {
+        self as u8
+    }
+}
+
+// Each page is described by the Page structure. Linux does this
+// as well, where each 4096-byte chunk of memory has a structure
+// associated with it. However, there structure is much larger.
+pub struct Page {
+    flags: u8,
+}
+
+impl Page {
+    // If this page has been marked as the final allocation,
+    // this function returns true. Otherwise, it returns false.
+    pub fn is_last(&self) -> bool {
+        if self.flags & PageBits::Last.val() != 0 {
+            true
+        }
+        else {
+            false
+        }
+    }
+
+    // If the page is marked as being taken (allocated), then
+    // this function returns true. Otherwise, it returns false.
+    pub fn is_taken(&self) -> bool {
+        if self.flags & PageBits::Taken.val() != 0 {
+            true
+        }
+        else {
+            false
+        }
+    }
+
+    // This is the opposite of is_taken().
+    pub fn is_free(&self) -> bool {
+        !self.is_taken()
+    }
+
+    // Clear the Page structure and all associated allocations.
+    pub fn clear(&mut self) {
+        self.flags = PageBits::Empty.val();
+    }
+
+    // Set a certain flag. We ran into trouble here since PageBits
+    // is an enumeration and we haven't implemented the BitOr Trait
+    // on it.
+    pub fn set_flag(&mut self, flag: PageBits) {
+        self.flags |= flag.val();
+    }
+
+    pub fn clear_flag(&mut self, flag: PageBits) {
+        self.flags &= !(flag.val());
+    }
+}
+
+/// Initialize the allocation system. There are several ways that we can
+/// implement the page allocator:
+/// 1. Free list (singly linked list where it starts at the first free
+/// allocation) 2. Bookkeeping list (structure contains a taken and length)
+/// 3. Allocate one Page structure per 4096 bytes (this is what I chose)
+/// 4. Others
+pub fn page_init() {
+    unsafe {
+        let num_pages = HEAP_SIZE / PAGE_SIZE;
+        let ptr = HEAP_START as *mut Page;
+        // Clear all pages to make sure that they aren't accidentally
+        // taken
+        for i in 0..num_pages {
+            (*ptr.add(i)).clear();
+        }
+        // Determine where the actual useful memory starts. This will be
+        // after all Page structures. We also must align the ALLOC_START
+        // to a page-boundary (PAGE_SIZE = 4096). ALLOC_START =
+        // (HEAP_START + num_pages * size_of::<Page>() + PAGE_SIZE - 1)
+        // & !(PAGE_SIZE - 1);
+        ALLOC_START = align_val(
+            HEAP_START
+                + num_pages * size_of::<Page,>(),
+            PAGE_ORDER,
+        );
+    }
+}
+
+/// Allocate a page or multiple pages
+/// pages: the number of PAGE_SIZE pages to allocate
+pub fn alloc(pages: usize) -> *mut u8 {
+    // We have to find a contiguous allocation of pages
+    assert!(pages > 0);
+    unsafe {
+        // We create a Page structure for each page on the heap. We
+        // actually might have more since HEAP_SIZE moves and so does
+        // the size of our structure, but we'll only waste a few bytes.
+        let num_pages = HEAP_SIZE / PAGE_SIZE;
+        let ptr = HEAP_START as *mut Page;
+        for i in 0..num_pages - pages {
+            let mut found = false;
+            // Check to see if this Page is free. If so, we have our
+            // first candidate memory address.
+            if (*ptr.add(i)).is_free() {
+                // It was FREE! Yay!
+                found = true;
+                for j in i..i + pages {
+                    // Now check to see if we have a
+                    // contiguous allocation for all of the
+                    // request pages. If not, we should
+                    // check somewhere else.
+                    if (*ptr.add(j)).is_taken() {
+                        found = false;
+                        break;
+                    }
+                }
+            }
+            // We've checked to see if there are enough contiguous
+            // pages to form what we need. If we couldn't, found
+            // will be false, otherwise it will be true, which means
+            // we've found valid memory we can allocate.
+            if found {
+                for k in i..i + pages - 1 {
+                    (*ptr.add(k)).set_flag(PageBits::Taken);
+                }
+                // The marker for the last page is
+                // PageBits::Last This lets us know when we've
+                // hit the end of this particular allocation.
+                (*ptr.add(i+pages-1)).set_flag(PageBits::Taken);
+                (*ptr.add(i+pages-1)).set_flag(PageBits::Last);
+                // The Page structures themselves aren't the
+                // useful memory. Instead, there is 1 Page
+                // structure per 4096 bytes starting at
+                // ALLOC_START.
+                return (ALLOC_START + PAGE_SIZE * i)
+                    as *mut u8;
+            }
+        }
+    }
+
+    // If we get here, that means that no contiguous allocation was
+    // found.
+    null_mut()
+}
+
+/// Allocate and zero a page or multiple pages
+/// pages: the number of pages to allocate
+/// Each page is PAGE_SIZE which is calculated as 1 << PAGE_ORDER
+/// On RISC-V, this typically will be 4,096 bytes.
+pub fn zalloc(pages: usize) -> *mut u8 {
+    // Allocate and zero a page.
+    // First, let's get the allocation
+    let ret = alloc(pages);
+    if !ret.is_null() {
+        let size = (PAGE_SIZE * pages) / 8;
+        let big_ptr = ret as *mut u64;
+        for i in 0..size {
+            // We use big_ptr so that we can force an
+            // sd (store doubleword) instruction rather than
+            // the sb. This means 8x fewer stores than before.
+            // Typically we have to be concerned about remaining
+            // bytes, but fortunately 4096 % 8 = 0, so we
+            // won't have any remaining bytes.
+            unsafe {
+                (*big_ptr.add(i)) = 0;
+            }
+        }
+    }
+    ret
+}
+
+/// Deallocate a page by its pointer
+/// The way we've structured this, it will automatically coalesce
+/// contiguous pages.
+pub fn dealloc(ptr: *mut u8) {
+    // Make sure we don't try to free a null pointer.
+    assert!(!ptr.is_null());
+    unsafe {
+        let addr =
+            HEAP_START + (ptr as usize - ALLOC_START) / PAGE_SIZE;
+        // Make sure that the address makes sense. The address we
+        // calculate here is the page structure, not the HEAP address!
+        assert!(addr >= HEAP_START && addr < HEAP_START + HEAP_SIZE);
+        let mut p = addr as *mut Page;
+        // Keep clearing pages until we hit the last page.
+        while (*p).is_taken() && !(*p).is_last() {
+            (*p).clear();
+            p = p.add(1);
+        }
+        // If the following assertion fails, it is most likely
+        // caused by a double-free.
+        assert!(
+            (*p).is_last() == true,
+            "Possible double-free detected! (Not taken found \
+		         before last)"
+        );
+        // If we get here, we've taken care of all previous pages and
+        // we are on the last page.
+        (*p).clear();
+    }
+}
+
+/// Print all page allocations
+/// This is mainly used for debugging.
+pub fn print_page_allocations() {
+    unsafe {
+        let num_pages = HEAP_SIZE / PAGE_SIZE;
+        let mut beg = HEAP_START as *const Page;
+        let end = beg.add(num_pages);
+        let alloc_beg = ALLOC_START;
+        let alloc_end = ALLOC_START + num_pages * PAGE_SIZE;
+        println!(
+            "PAGE ALLOCATION TABLE\nMETA: {:p} -> {:p}\nPHYS: \
+		          0x{:x} -> 0x{:x}",
+            beg, end, alloc_beg, alloc_end
+        );
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        let mut num = 0;
+        while beg < end {
+            if (*beg).is_taken() {
+                let start = beg as usize;
+                let memaddr = ALLOC_START
+                    + (start - HEAP_START)
+                    * PAGE_SIZE;
+                print!("0x{:x} => ", memaddr);
+                loop {
+                    num += 1;
+                    if (*beg).is_last() {
+                        let end = beg as usize;
+                        let memaddr = ALLOC_START
+                            + (end
+                            - HEAP_START)
+                            * PAGE_SIZE
+                            + PAGE_SIZE - 1;
+                        print!(
+                            "0x{:x}: {:>3} page(s)",
+                            memaddr,
+                            (end - start + 1)
+                        );
+                        println!(".");
+                        break;
+                    }
+                    beg = beg.add(1);
+                }
+            }
+            beg = beg.add(1);
+        }
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        println!(
+            "Allocated: {:>5} pages ({:>9} bytes).",
+            num,
+            num * PAGE_SIZE
+        );
+        println!(
+            "Free     : {:>5} pages ({:>9} bytes).",
+            num_pages - num,
+            (num_pages - num) * PAGE_SIZE
+        );
+    }
+}
+
+fn test_page_allocation() {
+    print_page_allocations();
+    let t1 = alloc(10);
+    print_page_allocations();
+    let t2 = alloc(8);
+    print_page_allocations();
+    dealloc(t1);
+    print_page_allocations();
+    dealloc(t2);
+    print_page_allocations();
+}
+
+//-----------------------------------------------------
 
 fn clear_bss() {
     extern "C" {
@@ -114,7 +417,15 @@ extern "C" fn rust_main() {
         boot_stack as usize, boot_stack_top as usize
     );
     println!(".stack [{:#x}, {:#x})", stack_begin as usize, stack_end as usize);
-    println!(".ekernel [{:#x})", ekernel as usize);
-    println!("user_begin [{:#x})", user_begin as usize);
+    println!(".ekernel {:#x}", ekernel as usize);
+    println!("user_begin {:#x}", user_begin as usize);
+    //-----------------------
+    unsafe {
+        HEAP_START=ekernel as usize +4096;
+        HEAP_SIZE = user_begin as usize - HEAP_START;
+        println!("HEAP_START {:#x}, HEAP_SIZE {:#x}", HEAP_START, HEAP_SIZE);
+    }
+    page_init();
+    test_page_allocation();
     panic!("It should shutdown!");
 }
