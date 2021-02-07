e01a60306305774eeee88135c70fc3011af23aa0
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-02-07 14:14:05 +0800

    add usrapp, and the usrapp is broken. ERR!
    $./run.sh
    Free     :  1476 pages (  6045696 bytes).
    [kernel] trap_handler Exception(Breakpoint), stval = 0x0, sepc = 0x8002040e
    Panicked at src/main.rs:732 Unsupported trap Exception(InstructionPageFault), stval = 0x806000d6, sepc = 0x806000d6

--- os/src/main.rs
+++ os/src/main.rs
@@ -769,6 +769,58 @@ pub fn do_syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     }
 }
 
+//================  run userapp ===================================
+const USER_STACK_SIZE: usize = 4096 * 2;
+const KERNEL_STACK_SIZE: usize = 4096 * 2;
+
+#[repr(align(4096))]
+struct KernelStack {
+    data: [u8; KERNEL_STACK_SIZE],
+}
+
+#[repr(align(4096))]
+struct UserStack {
+    data: [u8; USER_STACK_SIZE],
+}
+
+static KERNEL_STACK: KernelStack = KernelStack {
+    data: [0; KERNEL_STACK_SIZE],
+};
+static USER_STACK: UserStack = UserStack {
+    data: [0; USER_STACK_SIZE],
+};
+
+impl KernelStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + KERNEL_STACK_SIZE
+    }
+    pub fn push_context(&self, cx: TrapContext) -> &'static mut TrapContext {
+        let cx_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
+        unsafe {
+            *cx_ptr = cx;
+        }
+        unsafe { cx_ptr.as_mut().unwrap() }
+    }
+}
+
+impl UserStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + USER_STACK_SIZE
+    }
+}
+
+pub fn run_usrapp() -> ! {
+    extern "C" {
+        fn __restore(cx_addr: usize); //in trap.S
+    }
+    unsafe {
+        __restore(KERNEL_STACK.push_context(TrapContext::app_init_context(
+            usr_app_main as usize,
+            USER_STACK.get_sp(),
+        )) as *const _ as usize);
+    }
+    panic!("Unreachable in batch::run_current_app!");
+}
 //-------------------------------------------------------
 
 fn clear_bss() {
@@ -839,6 +891,8 @@ extern "C" fn rust_main() {
     // let ptr = 0x80600000 as *const u8;
     // unsafe{ let c = *ptr; println!("{}",c); }
     //--------------- test paging code -------------------
+
+    run_usrapp();
 
     panic!("\n[kernel] END: It should shutdown!");
 }
