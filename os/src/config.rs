b7600bffaf5321b5d135a01ae9d09c9d34cc3e88
Yifan Wu <shinbokuow@163.com>
2021-02-07 18:04:38 +0800

    Bump rustsbi to 0.1.1 && make config of qemu/k210 different

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,7 +1,7 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const MAX_APP_NUM: usize = 4;
-pub const APP_BASE_ADDRESS: usize = 0x80100000;
+pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
 #[cfg(feature = "board_k210")]
