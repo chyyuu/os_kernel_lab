bb11abaf734daaa70426fb139debc837652a35db
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-02-10 02:53:13 +0800

    test block device OK!

--- os/src/main.rs
+++ os/src/main.rs
@@ -681,8 +681,86 @@ pub fn test_alloc() {
     a.push(3);
     assert_eq!(a[0],3);
 }
-//--------------------------------------------------------------
+//--------------------block device------------------------------------------
+pub const MEMORY_END: usize = 0x80800000;
+pub const MEMORY_DMA: usize = MEMORY_END - 0x100000;
+pub const VIRTIO0: usize = 0x10001000;
 
+use virtio_drivers::{VirtIOBlk, VirtIOHeader};
+
+pub trait BlockDevice {
+    fn read_block(&mut self, block_id: usize, buf: &mut [u8]);
+    fn write_block(&mut self, block_id: usize, buf: &[u8]);
+}
+
+pub struct VirtIOBlock(VirtIOBlk<'static>);
+
+type BlockDeviceImpl = VirtIOBlock;
+
+pub fn test_block_device() {
+    let mut block_device = BlockDeviceImpl::new();
+    let mut write_buffer = [0u8; 512];
+    let mut read_buffer =  [0u8; 512];
+    for i in 0..512 {
+        for byte in write_buffer.iter_mut() { *byte = i as u8; }
+    }
+    block_device.write_block(0 as usize, &write_buffer);
+    block_device.read_block(0 as usize, &mut read_buffer);
+    assert_eq!(write_buffer, read_buffer);
+
+    println!("block device test passed!");
+}
+
+
+
+
+impl BlockDevice for VirtIOBlock {
+    fn read_block(&mut self, block_id: usize, buf: &mut [u8]) {
+        self.0.read_block(block_id, buf).expect("Error when reading VirtIOBlk");
+    }
+    fn write_block(&mut self, block_id: usize, buf: &[u8]) {
+        self.0.write_block(block_id, buf).expect("Error when writing VirtIOBlk");
+    }
+}
+
+impl VirtIOBlock {
+    pub fn new() -> Self {
+        Self(VirtIOBlk::new(
+            unsafe { &mut *(VIRTIO0 as *mut VirtIOHeader) }
+        ).expect("failed to create blk driver"))
+    }
+}
+
+use core::sync::atomic::*;
+
+static DMA_PADDR: AtomicUsize = AtomicUsize::new(MEMORY_DMA);
+
+#[no_mangle]
+extern "C" fn virtio_dma_alloc(pages: usize) -> PhysAddr {
+    let paddr = DMA_PADDR.fetch_add(0x1000 * pages, Ordering::SeqCst);
+    println!("alloc DMA: paddr={:#x}, pages={}", paddr, pages);
+    paddr
+}
+
+#[no_mangle]
+extern "C" fn virtio_dma_dealloc(paddr: PhysAddr, pages: usize) -> i32 {
+    println!("dealloc DMA: paddr={:#x}, pages={}", paddr, pages);
+    0
+}
+
+#[no_mangle]
+extern "C" fn virtio_phys_to_virt(paddr: PhysAddr) -> VirtAddr {
+    paddr
+}
+
+#[no_mangle]
+extern "C" fn virtio_virt_to_phys(vaddr: VirtAddr) -> PhysAddr {
+    vaddr
+}
+
+type VirtAddr = usize;
+type PhysAddr = usize;
+//---------------------------------------------------------------
 
 fn clear_bss() {
     extern "C" {
@@ -736,6 +814,7 @@ extern "C" fn rust_main() {
     test_page_allocation();
     kmem_init();
     test_alloc();
+    test_block_device();
     println!("[kernel] OK ALL! Shutdown!");
     shutdown();
     panic!("NO! should not come here");
