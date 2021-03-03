856f3a051512af4071433db769f5cfccf5f1755e
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-03-03 16:22:13 +0800

    try to change sp

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,6 +9,7 @@ const STACKSIZE: isize = 48;
 #[repr(C)]
 pub struct TaskContext {
     ra: usize,
+    sp: usize,
 }
 
 #[naked]
@@ -16,6 +17,7 @@ pub struct TaskContext {
 unsafe fn switch_(new: *const TaskContext) {
     llvm_asm!("
         ld ra, 0(a0)  //TaskContext.ra = f1 fun
+        ld sp, 8(a0)  //TaskContext.sp = f1 fun
         ret
         "
     :    :    :    :
@@ -40,12 +42,14 @@ fn main() {
     // 2. read f1 addr from new_stack to task_context.ra,
     // 3. switch to f1
     unsafe {
+        println!("f1:     {:#04x}",f1 as u64);
         let stack_bottom = stack.as_mut_ptr().offset(STACKSIZE);
         let stack_ptr = (stack_bottom as usize & !15) as *mut u8;
         ptr::write(stack_ptr.offset(STACKSIZE - 8) as * mut u64, f1 as u64);
         ctx.ra = ptr::read(stack_ptr.offset(STACKSIZE - 8) as * mut u64) as usize;
+        ctx.sp = ptr::read(stack_ptr.offset(STACKSIZE - 16) as * mut u64) as usize;
         //println!("ctx.ra: {:#04x}",ctx.ra);
-        //println!("f1:     {:#04x}",f1 as u64);
+
         switch_(&mut ctx);
     }
 
