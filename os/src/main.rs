fb5736e0137c5a251f0ca4bc32a1bac8228e017a
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2021-03-03 16:34:14 +0800

    test new stack OK

--- os/src/main.rs
+++ os/src/main.rs
@@ -17,7 +17,7 @@ pub struct TaskContext {
 unsafe fn switch_(new: *const TaskContext) {
     llvm_asm!("
         ld ra, 0(a0)  //TaskContext.ra = f1 fun
-        ld sp, 8(a0)  //TaskContext.sp = f1 fun
+        ld sp, 8(a0)  //TaskContext.sp = new stack
         ret
         "
     :    :    :    :
@@ -26,29 +26,32 @@ unsafe fn switch_(new: *const TaskContext) {
 
 
 fn f1() {
-    println!("Hello, world! in f1()");
+    let num = 16;
+    println!("Hello, world! num {} in f1()",num);
+    println!("Hello, world! &num {:#08x} in f1()",&num as *const i32 as  usize );
     loop {}
 }
 
 fn main() {
 
     println!("Hello, world! in main()");
-    f1();
+    //f1();
 
     let mut ctx = TaskContext::default();
     let mut stack = vec![0_u8; STACKSIZE as usize];
-
+    println!("stack:     {:#08x}",stack.as_ptr() as u64);
     // 1. push f1 addr in new_stack,
     // 2. read f1 addr from new_stack to task_context.ra,
     // 3. switch to f1
     unsafe {
-        println!("f1:     {:#04x}",f1 as u64);
+        println!("f1:     {:#08x}",f1 as u64);
         let stack_bottom = stack.as_mut_ptr().offset(STACKSIZE);
         let stack_ptr = (stack_bottom as usize & !15) as *mut u8;
         ptr::write(stack_ptr.offset(STACKSIZE - 8) as * mut u64, f1 as u64);
+        ptr::write(stack_ptr.offset(STACKSIZE - 16) as * mut u64,  stack_ptr as u64 - 16);
         ctx.ra = ptr::read(stack_ptr.offset(STACKSIZE - 8) as * mut u64) as usize;
         ctx.sp = ptr::read(stack_ptr.offset(STACKSIZE - 16) as * mut u64) as usize;
-        //println!("ctx.ra: {:#04x}",ctx.ra);
+        println!("ctx.ra: {:#08x}",ctx.ra);
 
         switch_(&mut ctx);
     }
