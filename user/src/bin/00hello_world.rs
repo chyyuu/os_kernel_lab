5369766b968d3870a2cee4277be2b5e434a590b7
Yifan Wu <shinbokuow@163.com>
2021-03-09 16:02:13 +0800

    Bump rustsbi to 0.2.0-alpha.1[81d53d8]

--- user/src/bin/00hello_world.rs
+++ user/src/bin/00hello_world.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(llvm_asm)]
 
 #[macro_use]
 extern crate user_lib;
@@ -7,5 +8,8 @@ extern crate user_lib;
 #[no_mangle]
 fn main() -> i32 {
     println!("Hello, world!");
+    unsafe {
+        llvm_asm!("sret");
+    }
     0
 }
