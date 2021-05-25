5057b47bfa69d0a54dc5fd4d0f4152c14341c2c9
Mark <etyyuiope@qq.com>
2021-05-25 20:12:22 +0800

    Update Lab4

--- labcodes/lab4/kern/mm/swap_fifo.c
+++ labcodes/lab4/kern/mm/swap_fifo.c
@@ -50,6 +50,7 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
     assert(entry != NULL && head != NULL);
     //record the page access situlation
     /*LAB3 EXERCISE 2: YOUR CODE*/ 
+    list_add(head, entry);
     //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
     return 0;
 }
@@ -67,6 +68,12 @@ _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick
      /*LAB3 EXERCISE 2: YOUR CODE*/ 
      //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
      //(2)  assign the value of *ptr_page to the addr of this page
+     list_entry_t *le = head->prev;
+    assert(head != le);
+    struct Page *p = le2page(le, pra_page_link);
+    list_del(le);
+    assert(p != NULL);
+    *ptr_page = p;
      return 0;
 }
 
