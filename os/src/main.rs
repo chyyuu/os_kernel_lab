4f834ef5c9b4a11cded64582a8541eac7a27c6dc
Yifan Wu <shinbokuow@163.com>
2021-07-09 08:10:39 +0800

    Modify clear_bss

--- os/src/main.rs
+++ os/src/main.rs
@@ -21,9 +21,12 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| {
-        unsafe { (a as *mut u8).write_volatile(0) }
-    });
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as usize as *mut u8, 
+            ebss as usize - sbss as usize,
+        ).fill(0);
+    }
 }
 
 #[no_mangle]
