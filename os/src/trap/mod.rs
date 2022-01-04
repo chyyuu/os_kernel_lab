e0657a71300027ff7515d78355403d6b01d2d03f
Yifan Wu <shinbokuow@163.com>
2022-01-03 19:41:01 -0800

    Kernel cannot dump now.

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -34,11 +34,11 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
         }
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
-            println!("[kernel] PageFault in application, killed by kernel.");
+            println!("[kernel] PageFault in application, kernel killed it.");
             run_next_app();
         }
         Trap::Exception(Exception::IllegalInstruction) => {
-            println!("[kernel] IllegalInstruction in application, killed by kernel.");
+            println!("[kernel] IllegalInstruction in application, kernel killed it.");
             run_next_app();
         }
         _ => {
