74dd4b628407462deb76b1cadfb68e0c38ccb5cf
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2022-03-25 15:12:24 +0800

    add #![deny(missing_docs)] AND #![deny(warnings)] in main.rs, and update comments in other files

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,3 +1,5 @@
+//! The panic handler
+
 use crate::sbi::shutdown;
 use core::panic::PanicInfo;
 
@@ -5,13 +7,13 @@ use core::panic::PanicInfo;
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
         println!(
-            "Panicked at {}:{} {}",
+            "[kernel] Panicked at {}:{} {}",
             location.file(),
             location.line(),
             info.message().unwrap()
         );
     } else {
-        println!("Panicked: {}", info.message().unwrap());
+        println!("[kernel] Panicked: {}", info.message().unwrap());
     }
     shutdown()
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,14 @@
+//! The main module and entrypoint
+//!
+//! The operating system and app also starts in this module. Kernel code starts
+//! executing from `entry.asm`, after which [`rust_main()`] is called to
+//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! details.)
+//!
+//! We then call [`println!`] to display `Hello, world!`.
+
+#![deny(missing_docs)]
+#![deny(warnings)]
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
@@ -11,6 +22,7 @@ mod sbi;
 
 global_asm!(include_str!("entry.asm"));
 
+/// clear BSS segment
 fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -19,6 +31,7 @@ fn clear_bss() {
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
+/// the rust entry-point of os
 #[no_mangle]
 pub fn rust_main() -> ! {
     extern "C" {
