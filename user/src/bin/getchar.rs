40c84071c0e372b343d1c4c0d90cc925111d2c9b
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2022-05-07 11:36:06 +0800

    add usr app: getchar

---
+++ user/src/bin/getchar.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+#[macro_use]
+extern crate user_lib;
+use user_lib::console::getchar;
+
+const LF: u8 = 0x0au8;
+const CR: u8 = 0x0du8;
+
+#[no_mangle]
+pub fn main() -> i32 {
+    println!("getchar starting....  Press 'ENTER' will quit.");
+
+    loop {
+        let c = getchar();
+
+        println!("Got Char  {}", c);
+        if c == LF || c==CR  {
+            println!("exit(0)");
+            return 0;
+        }
+    }
+}
