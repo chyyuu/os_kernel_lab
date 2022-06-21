bc2aec976445e265cfa8702cce53a2127210cedc
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2022-06-22 00:23:33 +0800

    add VIRT_TEST MMIO range (0x0010_0000, 0x00_2000)

--- os/src/boards/qemu.rs
+++ os/src/boards/qemu.rs
@@ -1,7 +1,7 @@
 pub const CLOCK_FREQ: usize = 12500000;
 
 pub const MMIO: &[(usize, usize)] = &[
-    //    (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
+    (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
     (0x2000000, 0x10000),
     (0xc000000, 0x210000), // VIRT_PLIC in virt machine
     (0x10000000, 0x9000),  // VIRT_UART0 with GPU  in virt machine
