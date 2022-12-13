652e289730fc0d6b015cfe606686dad302b1181f
Yifan Wu <shinbokuow@163.com>
2022-12-13 22:48:37 +0800

    Remove K210 support.

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -144,14 +144,8 @@ impl TaskManager {
             // go back to user mode
         } else {
             println!("All applications completed!");
-
-            #[cfg(feature = "board_qemu")]
             use crate::board::QEMUExit;
-            #[cfg(feature = "board_qemu")]
             crate::board::QEMU_EXIT_HANDLE.exit_success();
-
-            #[cfg(feature = "board_k210")]
-            panic!("All applications completed!");
         }
     }
 }
