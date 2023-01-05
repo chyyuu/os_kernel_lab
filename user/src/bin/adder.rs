be793ce907ae9a3d3a1fd5c0c6193fdf3c438817
Yifan Wu <shinbokuow@163.com>
2023-01-05 22:25:16 +0800

    Update adder

--- user/src/bin/adder.rs
+++ user/src/bin/adder.rs
@@ -9,8 +9,9 @@ use alloc::vec::Vec;
 use user_lib::{exit, get_time, thread_create, waittid};
 
 static mut A: usize = 0;
-const PER_THREAD: usize = 10000;
-const THREAD_COUNT: usize = 16;
+const PER_THREAD_DEFAULT: usize = 10000;
+const THREAD_COUNT_DEFAULT: usize = 16;
+static mut PER_THREAD: usize = 0;
 
 unsafe fn critical_section(t: &mut usize) {
     let a = &mut A as *mut usize;
@@ -30,16 +31,25 @@ unsafe fn f() -> ! {
 }
 
 #[no_mangle]
-pub fn main() -> i32 {
+pub fn main(argc: usize, argv: &[&str]) -> i32 {
+    let mut thread_count = THREAD_COUNT_DEFAULT;
+    let mut per_thread = PER_THREAD_DEFAULT;
+    if argc >= 2 {
+        thread_count = argv[1].parse().unwrap();
+        if argc >= 3 {
+            per_thread = argv[2].parse().unwrap();
+        }
+    }
+    unsafe { PER_THREAD = per_thread; }
     let start = get_time();
     let mut v = Vec::new();
-    for _ in 0..THREAD_COUNT {
+    for _ in 0..thread_count {
         v.push(thread_create(f as usize, 0) as usize);
     }
     for tid in v.into_iter() {
         waittid(tid);
     }
     println!("time cost is {}ms", get_time() - start);
-    assert_eq!(unsafe { A }, PER_THREAD * THREAD_COUNT);
+    assert_eq!(unsafe { A }, unsafe { PER_THREAD } * thread_count);
     0
 }
