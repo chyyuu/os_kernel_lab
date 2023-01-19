8c09e0537ecd865ce87482359814428ce2aa0acf
Yifan Wu <shinbokuow@163.com>
2023-01-20 01:54:13 +0800

    add_task->wakeup_task

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -3,7 +3,7 @@ use core::cmp::Ordering;
 use crate::config::CLOCK_FREQ;
 use crate::sbi::set_timer;
 use crate::sync::UPIntrFreeCell;
-use crate::task::{add_task, TaskControlBlock};
+use crate::task::{wakeup_task, TaskControlBlock};
 use alloc::collections::BinaryHeap;
 use alloc::sync::Arc;
 use lazy_static::*;
@@ -64,7 +64,7 @@ pub fn check_timer() {
     TIMERS.exclusive_session(|timers| {
         while let Some(timer) = timers.peek() {
             if timer.expire_ms <= current_ms {
-                add_task(Arc::clone(&timer.task));
+                wakeup_task(Arc::clone(&timer.task));
                 timers.pop();
             } else {
                 break;
